527ca7e5ba3ae2e5ffe2e94ee02da87db8d5451d
saurabh <saurabh.rd80@gmail.com>
2025-07-12 22:53:40 +0530

    insert data in the db

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,21 @@ public class Main {
         String url = "jdbc:mysql://localhost:3306/aliens";
         String uname = "root";
         String password = "0000";
-        String query = "Select * from student";
+//        String query = "Select * from student";
+        String query1 = "insert into student(id,name)" +
+                "values" +
+                "(6,'bob')";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection(url, uname, password);
         Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(query);
-        while (rs.next()) {
-            String name = rs.getInt(1) + ":" + rs.getString(2);
-            System.out.println(name);
-        }
+//        ResultSet rs = st.executeQuery(query);
+        int count = st.executeUpdate(query1);
+        System.out.println(count + "rows affect");
+//        while (rs.next()) {
+//            String name = rs.getInt(1) + ":" + rs.getString(2);
+//            System.out.println(name);
+//        }
         st.close();
         con.close();
     }
